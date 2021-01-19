6875202f3237b2a3db513edffb9c445b1bcfb06d
Marcelo Silva <marcelo.silva@per.gov.ie>
2021-01-19 20:57:58 +0000

    adding files for github

---
+++ src/components/search-box/searchbox.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './searchbox.style.css';
+
+export const SearchBox = ({ placeholder, handleChange }) => (
+    <input type='search'
+        className='search'
+        placeholder={placeholder}
+        onChange={handleChange} />
+)
